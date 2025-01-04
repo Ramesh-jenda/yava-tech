4ea6fd1ab2b5358a1ae23230ff1e02551a2dcab4
Ramesh <ramesh.jenda99@gmail.com>
2025-01-04 19:30:02 +0530

    files

---
+++ blocks/footer.php
@@ -0,0 +1,140 @@
+
+    <!--==============================
+        FOOTER START
+    ===============================-->
+    <div class="home_2">
+        <footer class="footer_2">
+            <div class="container">
+                <div class="footer_body pt_75 pb_75 xs_pt_50 xs_pb_50">
+                    <div class="row justify-content-between">
+                        <div class="col-lg-3 col-md-8">
+                            <div class="footer_content xs_mb_50">
+                                <a class='footer_logo' href='index'>
+                                    <img src="images/Footer-logo.png" alt="IT-Hub" class="img-fluid w-100" />
+                                </a>
+                                <p class="footer_description pt_40">
+                                Yava Tech was established in the year 2023 Headquartered in Houston, TX, USA, and India, we are a preferred partner to global corporations like Synechrone, Nagarro, Dexian, and several others.
+                                </p>
+                                <ul class="social_link d-flex flex-wrap">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-sm-6 col-md-4">
+                            <div class="footer_content xs_mb_50">
+                                <h3>Services</h3>
+                                <ul class="footer_link">
+                                    <li><a href="it-staffing">IT Staffing Services</a></li>
+                                    <li><a href="rpo-service">Recruitment Process Outsourcing</a></li>
+                                    <li><a href="corporate-training">Corporate Trainings</a></li>
+                                    <li><a href="project-management-service">Project Management Services</a></li>
+                                    <li><a href="hr-consulting-service">HR Consulting Services</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-sm-6 col-md-4">
+                            <div class="footer_content xs_mb_50">
+                                <h3>Quick Links</h3>
+                                <ul class="footer_link">
+                                    <li><a href="about">About Us</a></li>
+                                    <li><a href="contact">Contact Us</a></li>
+                                    <li><a href="#">FAQs</a></li>
+                                    <li><a href="#">Terms and conditions</a></li>
+                                    <li><a href="#">Privacy policy</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-8">
+                            <div class="footer_content">
+                                <h3>Gallery</h3>
+                                <ul class="footer_gallery">
+                                    <li>
+                                        <img src="images/home2_foter_g-1.jpg" alt="image" class="img-fluid w-100">
+                                    </li>
+                                    <li>
+                                        <img src="images/home2_foter_g-2.jpg" alt="image" class="img-fluid w-100">
+                                    </li>
+                                    <li>
+                                        <img src="images/home2_foter_g-3.jpg" alt="image" class="img-fluid w-100">
+                                    </li>
+                                    <li>
+                                        <img src="images/home2_foter_g-4.jpg" alt="image" class="img-fluid w-100">
+                                    </li>
+                                    <li>
+                                        <img src="images/home2_foter_g-5.jpg" alt="image" class="img-fluid w-100">
+                                    </li>
+                                    <li>
+                                        <img src="images/home2_foter_g-6.jpg" alt="image" class="img-fluid w-100">
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer_bottom pt_45 pb_45 xs_pt_25 xs_pb_25">
+                    <div class="row ">
+                        <div class="col-xl-6 col-lg-5">
+                            <div class="copy">
+                                <p>Copyright &copy;2025 Yava Tech, All rights reserved.</p>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-7">
+                            <div class="footer_right_bottom">
+                                <ul>
+                                    <li><a href="#">FAQs</a></li>
+                                    <li><a href="#">Terms and conditions</a></li>
+                                    <li><a href="#">Privacy policy</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <!--==============================
+        FOOTER END
+    ===============================-->
+
+
+    <!--==============================
+        SCROLL BUTON START
+    ===============================-->
+    <div class="scroll_btn">
+        <i class="fas fa-chevron-up"></i>
+    </div>
+    <!--==============================
+        SCROLL BUTON END
+    ===============================-->
+
+
+    <!--jquery library js-->
+    <script src="js/jquery-3.7.0.min.js"></script>
+    <!--bootstrap js-->
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <!--font-awesome js-->
+    <script src="js/Font-Awesome.js"></script>
+    <!--venobox js-->
+    <script src="js/venobox.min.js"></script>
+    <!--isotope js-->
+    <script src="js/isotope.pkgd.min.js"></script>
+    <!--slick js-->
+    <script src="js/slick.min.js"></script>
+    <!--jquery.marquee js-->
+    <script src="js/jquery.marquee.min.js"></script>
+    <!--animated_barfiller js-->
+    <script src="js/animated_barfiller.js"></script>
+    <!--sticky_sidebar js-->
+    <script src="js/sticky_sidebar.js"></script>
+    <!--counter js-->
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.countup.min.js"></script>
+    <!--wow js-->
+    <script src="js/wow.min.js"></script>
+    <!--main/custom js-->
+    <script src="js/main.js"></script>
+</body>
+</html>
---
+++ blocks/header.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>IT Staffing, RPO, Corporate Training & HR Consulting Services</title>
+    <meta name="description" content="Discover expert IT staffing, recruitment process outsourcing, corporate training, project management, and HR consulting services to empower your business success.">
+    <link rel="icon" type="image/png" href="images/favicon.png" />
+    <link rel="stylesheet" href="css/all.min.css" />
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/venobox.min.css" />
+    <link rel="stylesheet" href="css/slick.css" />
+    <link rel="stylesheet" href="css/animated_barfiller.css" />
+    <link rel="stylesheet" href="css/animate.css" />
+    <link rel="stylesheet" href="css/utilities.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/responsive.css" />
+</head>
+
+<body>
+
+    <!--============================
+        MENU 2 START
+    =============================-->
+    <div class="home_2">
+        <nav class="navbar navbar-expand-lg main_menu">
+            <div class="container">
+                <a class='navbar-brand' href='index'>
+                    <img src="images/Logo.png" alt="IT-Hub" class="img-fluid w-100">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <i class="far fa-bars menu_bar_icon"></i>
+                    <i class="far fa-times menu_close_icon"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav m-auto">
+                        <li class="nav-item">
+                            <a aria-current='page' class='nav-link active' href='index'>Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class='nav-link' href='about'>About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Services <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg></a>
+                            <ul class="droap_menu">
+                                <li><a href='it-staffing'>IT Staffing Services
+                                </a></li>
+                                <li><a href='rpo-service'>Recruitment Process Outsourcing
+                                </a></li>
+                                <li><a href='corporate-training'>Corporate Trainings</a></li>
+                                <li><a href='project-management-service'>Project Management Services
+                                </a></li>
+                                <li><a href=''>Project Management Services
+                                </a></li>
+                                
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class='nav-link' href='contact'>Contact</a>
+                        </li>
+                    </ul>
+                    <div class="get_a_quote">
+                        <a class='common_btn' href='contact'>Get A Quote</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <!--============================
+        MENU 2 END
+    =============================-->
